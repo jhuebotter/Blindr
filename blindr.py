9ddd7ca19aa440756fbaffea845c58bc04e130d4
jhuebotter <hybi_357@live.de>
2018-12-08 05:34:27 +0100

    combined

--- blindr.py
+++ blindr.py
@@ -5,33 +5,171 @@ import sys
 import numpy as np
 import numbers
 import pandas as pd
+from scipy import stats
 import matplotlib.pyplot as plt
 from matplotlib import style
-import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from datetime import datetime
 import errno
-#import ontology as on
+from scipy.spatial import distance
+import ontology as on
+
 
 style.use('ggplot')
+np.random.seed(42)
 
 SCRIPT_PATH = os.getcwd()
 DATA_PATH = SCRIPT_PATH + '\input'
 OUTPUT_PATH = SCRIPT_PATH + '\output'
 FILE_NAME = 'example_data.xlsx'
-GROUPS = ['A','B','C']
+N_GROUPS = 3
+
 
 def main():
 
     file_path = '%s\%s' % (DATA_PATH, FILE_NAME)
     data = load_excel(file_path)
+    original_data = data.copy()
     data, features = process_data(data)
     data = standardscale(data)
-    plt = plot_data(data)
-    save_df(data, 'standardized_%s' % FILE_NAME)
+    #plt = plot_data(data)
+    #save_df(data, 'standardized_%s' % FILE_NAME)
     #plt.show()
-    animals make_animals(data, features)
-    return animals
+    animals = make_animals(data, features)
+    groups = make_groups(features)
+    assign_animals(animals, groups)
+    output = make_output(original_data, animals)
+    plt = plot_result(output)
+    output = blind_output(output)
+    save_df(output, 'grouped_%s' % FILE_NAME)
+    #plt.show()
+
+
+def blind_output(data):
+    drop = data.columns.values[2:-1]
+    new = [column for column in data.columns.values if column not in drop]
+    data = data.loc[:, data.columns.isin(new)]
+    print('output blinded!')
+    
+    return data
+
+
+def make_output(data, animals):
+    output = data[data.columns[:]]
+    output['Group'] = None
+    for animal in animals:
+        output['Group'].loc[animal.id] = animal.group_id+1
+    
+    return output
+
+
+def assign_animals(animals, groups):
+    unassigned_animals = [animal for animal in animals if animal.group_id==None]
+    while len(unassigned_animals) > 0:
+        largest_distance = 0.0
+        group_id = 0
+        animal_id = 0
+        for animal in unassigned_animals:
+            for group in groups:
+                dist = distance.euclidean(animal.matrix, group.mean_matrix)
+                #print(animal.id,group.id, dist)
+                if dist > largest_distance:
+                    largest_distance = dist
+                    group_pick = group
+                    animal_pick = animal
+        group_pick.assign(animal_pick)
+        unassigned_animals = [animal for animal in animals if animal.group_id==None]
+    equalize_groups(animals, groups)
+
+
+def equalize_groups(animals, groups):
+    mean_p = 0.0
+    largest_group_id = None
+    largest_group_size = 0
+    smallest_group_id = None
+    smallest_group_size = 100000000
+    for group in groups:
+        print()
+        print('Group ID:',group.id)
+        print('Group size:',len(group.animals))
+        if len(group.animals) > largest_group_size:
+            largest_group_size = len(group.animals)
+            largest_group_id = group.id
+        if len(group.animals) < smallest_group_size:
+            smallest_group_size = len(group.animals)
+            smallest_group_id = group.id
+        for other_group in groups:
+            if group == other_group:
+                pass
+            else:
+                t, p = compare_groups(group,other_group)
+                mean_p += p
+                print('Difference between group', group.id, 'and', other_group.id,':',p)
+    mean_p /= len(groups)*(len(groups)-1)
+    print('mean difference:',mean_p)
+    print('largest group:', largest_group_id)
+    print('smallest group:', smallest_group_id)
+    while largest_group_size - smallest_group_size > 1:
+        print('Group sizes are not balanced!')
+        many_animals = groups[largest_group_id].animals
+        best_mean_p = 0.0
+        best_animal = None
+        for animal in many_animals:
+            groups[largest_group_id].release(animal)
+            groups[smallest_group_id].assign(animal)
+            t, p = compare_groups(groups[largest_group_id],groups[smallest_group_id])
+            #print(animal.id,p)
+            if p > best_mean_p:
+                best_mean_p = p
+                best_animal = animal
+            groups[smallest_group_id].release(animal)
+            groups[largest_group_id].assign(animal)
+        print('best animal to switch is:',best_animal.id, 'with new mean p =', best_mean_p)
+        groups[largest_group_id].release(best_animal)
+        groups[smallest_group_id].assign(best_animal)
+
+        largest_group_id = None
+        largest_group_size = 0
+        smallest_group_id = None
+        smallest_group_size = 100000000
+        for group in groups:
+            print()
+            print('Group ID:',group.id)
+            print('Group size:',len(group.animals))
+            if len(group.animals) > largest_group_size:
+                largest_group_size = len(group.animals)
+                largest_group_id = group.id
+            if len(group.animals) < smallest_group_size:
+                smallest_group_size = len(group.animals)
+                smallest_group_id = group.id
+    else:
+        print('Group sizes are balanced!')
+        for group in groups:
+            print('Group ID:',group.id)
+            print('Group size:',len(group.animals))
+            for other_group in groups:
+                if group == other_group:
+                    pass
+                else:
+                    t, p = compare_groups(group,other_group)
+                    mean_p += p
+                    print('Difference between group', group.id, 'and', other_group.id,':',p)
+        return
+
+def compare_groups(group1,group2):
+    matrix1 = group1.full_matrix
+    matrix2 = group2.full_matrix
+    t, p = stats.ttest_ind(matrix1, matrix2, axis=None, equal_var=False)
+    #print(t, p)
+    return t, p
+
+
+def make_groups(features):
+    groups = []
+    for i in range(N_GROUPS):
+        groups.append(on.Group(i,len(features)))
+    
+    return groups
 
 
 def make_animals(data, features):
@@ -40,12 +178,7 @@ def make_animals(data, features):
         kwargs={}
         for key, value in data.loc[animal_id][features].iteritems():
             kwargs[key] = value
-        #print('Animal ID:',animal_id)
-        #print('Features:',kwargs)
-        #print()
-        animals.append([animal_id, kwargs])
-    print(animals)
-        #animals.append(on.Animal(animal_id,**kwargs))
+        animals.append(on.Animal(animal_id,**kwargs))
 
     return animals
 
@@ -63,6 +196,7 @@ def standardscale(df):
         except (RuntimeError, TypeError, NameError, ValueError) as err:
             print(str(datetime.now()), "could not scale feature due to: ", err)
             continue
+    
     return df
 
 
@@ -81,11 +215,9 @@ def process_data(data):
                 print('numerizing category:',category)
                 data[category] = (data[f] == category).astype(float)
                 vector_features.append(category)
-                #data[category] = data[category]/n# len(data[f].unique())
-            #print(data.head())
         else:
             vector_features.append(f)
-        #print(vector_features)
+    
     return data, vector_features
 
 
@@ -97,13 +229,36 @@ def plot_data(data):
             data[f].value_counts().plot(kind='bar',ax=ax[c],title=f)
         else:
             data[f].plot(kind='hist',ax=ax[c],title=f,bins=9)
-    fig.tight_layout()
+    plt.tight_layout()
+    
+    return plt
+
+
+def plot_result(data,savefig=True,show=False):
+    features = [x for x in data.columns[2:-1]]
+    n_groups = len(data['Group'].unique())
+    fig, ax = plt.subplots(1,len(features),figsize=(15, 4))
+    for c, f in enumerate(features):
+        if data[f].dtypes == object:
+            data.groupby(by='Group')[f].value_counts().unstack().plot.bar(ax=ax[c])
+        else:
+            data.boxplot(column=[f], by='Group', ax=ax[c])
+    plt.tight_layout()
+    if savefig:
+        name = "%s\grouped_%s.png" % (OUTPUT_PATH, FILE_NAME.split('.')[0])
+        plt.savefig(name)
+        print("saving plot as", name)
+    if show:
+        plt.show()
+        return
+    
     return plt
 
 
 def load_excel(path=DATA_PATH):
     data = pd.read_excel(path)
     data.set_index('ID', inplace=True)
+    
     return data
 
 
@@ -113,7 +268,7 @@ def save_df(df, name, fullpath=False):
         df.to_excel('%s' % (name))
     else:
         mkdir_p(OUTPUT_PATH)
-        print("saving df as ", OUTPUT_PATH, name)
+        print("saving data as %s\%s" % (OUTPUT_PATH, name))
         df.to_excel('%s\%s' % (OUTPUT_PATH, name))
     print("saving succuessful!")
 
--- ontology.py
+++ ontology.py
@@ -1,53 +1,67 @@
 import numpy as np
-
-kwargs = {'weight':23,"speed":95000,"actv":4500}
-kwargs2 = {'weight':24,"speed":89000,"actv":3500}
-kwargs3 = {'weight':25,"speed":76000,"actv":6500}
-
+from scipy import stats
 
 
 class Animal:
-    id = ""
-    matrix = []
 
-    def __init__(self,id,**kwargs):
-        self.id = id
+    def __init__(self,id_no,**kwargs):
+        self.group_id = None
+        self.id = id_no
+        self.matrix = []
         for key,value in kwargs.items():
-            print(key,value)
             setattr(self, key, value)
             self.matrix.append(value)
         self.matrix = np.array(self.matrix)
-        print("A clone has been made")
+        print("created animal with ID:", self.id)
 
 
 class Group:
-    id = ""
-    animals = []
-    mean_matrix = []
 
-    def __init__(self,id_no):
+    def __init__(self,id_no,n_feat):
+        self.animals = []
         self.id = id_no
+        self.n_feat = n_feat
+        self.full_matrix = []
+        self.mean_matrix = np.zeros(self.n_feat)
+        self.sem_matrix = np.zeros(self.n_feat)
+        self.animals_list = []
+        print("created group with ID:", self.id)
+
+    def assign(self, anim):
+        self.animals.append(anim)
+        #print("assigned animal", anim.id, "to group", self.id)
+        anim.group_id = self.id
+        self.update_mean_features()
+
+    def release(self, anim):
+        self.animals.remove(anim)
+        #print("removed animal", anim.id, "from group", self.id)
+        anim.group_id = None
+        self.update_mean_features()     
 
     def update_mean_features(self):
-        for i in range(0,len( self.animals )):
-            self.mean_matrix.append( self.animals[i].matrix )
-        print(self.mean_matrix)
+        self.animals_list = [x.id for x in self.animals]
+        self.full_matrix = []
+        self.mean_matrix = []
+        self.sem_matrix = []
+        for animal in self.animals:
+            self.full_matrix.append(animal.matrix)
+        self.sem_matrix = np.matrix(stats.sem(self.full_matrix))
+        self.mean_matrix = np.matrix(self.full_matrix).mean(0)
 
 
-
-
-
+'''
 class Experimental_group:
     name = ""
     groups = []
 
     def __init__(self,id,no_of_groups):
-        self.id = id
+        self.id = id    
         for i in range (no_of_groups):
             group = Group(i)
             self.groups.append(group)
 
-    def assign(self, anim,flag):
+    def assign(self, anim, flag):
         print("how many groups ",len(self.groups))
 
         ###only the first times
@@ -55,24 +69,26 @@ class Experimental_group:
             if not self.groups[i].animals:
                 self.groups[i].animals.append(anim)
                 print(i)
-
                 break
 
         if flag is True:
             self.groups[0].animals.append(anim)
 
+'''
 
 
 
 
-
-
+'''
 #TEST
+kwargs = {'weight':23,"speed":95000,"actv":4500}
+kwargs2 = {'weight':24,"speed":89000,"actv":3500}
+kwargs3 = {'weight':25,"speed":76000,"actv":6500}
+
 mouse = Animal("01", **kwargs)
 mouse2 = Animal("02", **kwargs2)
 mouse3 = Animal("03", **kwargs3)
 mouse4 = Animal("03", **kwargs3)
-
 
 experiment = Experimental_group("test_case", 2)
 
@@ -81,10 +97,9 @@ experiment.assign(mouse2,False)
 experiment.assign(mouse3, True)
 experiment.assign(mouse4, True)
 
-
-
 experiment.groups[0].update_mean_features()
 
 
 
 #print(vars(mouse))
+'''
